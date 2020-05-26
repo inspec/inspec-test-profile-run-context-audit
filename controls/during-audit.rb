a2cd635803fa0bf94f6143ebdfa9d6e64fa0d45d
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-05-25 20:26:42 -0400

    Marshal out the stack during audit for later analysis
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- controls/during-audit.rb
+++ controls/during-audit.rb
@@ -7,10 +7,22 @@ control "json-stack" do
   end
 end
 
-require "inspec/utils/telemetry/run_context_probe"
 
-control "run-context" do
-  describe Inspec::Telemetry::RunContextProbe.guess_run_context do
-    it { should cmp "audit-cookbook" }
-  end
-end
+# This is the sensible thing to do, but we can't do this until the RunContext
+# feature is released and included with Chef Workstation.
+# Why? Because the audit cookbook always uses the embedded inspec gem.
+
+# require "inspec/utils/telemetry/run_context_probe"
+# control "run-context" do
+#   describe Inspec::Telemetry::RunContextProbe.guess_run_context do
+#     it { should cmp "audit-cookbook" }
+#   end
+# end
+
+# So instead, capture the stack to a file, and let the verify-stage
+# "after-audit" profile handle checking the stack.
+# Unfortunately, the JSON dump is lossy, and there is no direct Marshal implementation.
+# So we have to make one.
+Frame = Struct.new(:absolute_path, :label)
+marshallable_stack = stack.map { |f| Frame.new(f.absolute_path, f.label) }
+File.write("/tmp/audit_stack.dat", Marshal.dump(marshallable_stack))
